1da35acef56fbc5a83a3e5b4ba972e2aeff53c03
Petr Zemek <s3rvac@gmail.com>
2017-04-13 10:55:13 +0200

    Refactor fileinfo argument parsing into a separate function.

--- src/tools/fileinfo.rs
+++ src/tools/fileinfo.rs
@@ -3,8 +3,9 @@
 use std::path::Path;
 
 use clap::App;
-use clap::Arg;
 use clap::AppSettings;
+use clap::Arg;
+use clap::ArgMatches;
 
 use analysis::AnalysisArguments;
 use error::Result;
@@ -12,8 +13,8 @@ use fileinfo::Fileinfo;
 use settings::Settings;
 use VERSION;
 
-fn run() -> Result<()> {
-    let matches = App::new("fileinfo")
+fn parse_args<'a>() -> ArgMatches<'a> {
+    App::new("fileinfo")
         .version(VERSION)
         .about("Analyzes the given binary file via retdec.com's API.")
         .after_help("The output from the analysis is printed to the standard output.")
@@ -37,16 +38,20 @@ fn run() -> Result<()> {
             // It is important not to require the API URL by default because it
             // enables the use of the RETDEC_API_URL environment variable.
             .help("Custom URL to the retdec.com's API."))
-        .get_matches();
+        .get_matches()
+}
+
+fn run() -> Result<()> {
+    let args = parse_args();
 
     let mut settings = Settings::new();
-    if let Some(api_key) = matches.value_of("api_key") {
+    if let Some(api_key) = args.value_of("api_key") {
         settings = settings.with_api_key(api_key);
     }
-    if let Some(api_url) = matches.value_of("api_url") {
+    if let Some(api_url) = args.value_of("api_url") {
         settings = settings.with_api_url(api_url);
     }
-    let input_file = matches.value_of("FILE")
+    let input_file = args.value_of("FILE")
         .expect("clap did not properly handle the absence of FILE");
 
     let fileinfo = Fileinfo::new(settings);
