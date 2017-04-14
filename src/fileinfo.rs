5a00309159edba6a4b4157742de195abc3da6c31
Petr Zemek <s3rvac@gmail.com>
2017-04-14 09:51:56 +0200

    Refactor the creation of APIArguments in Fileinfo::start_analysis().

--- src/fileinfo.rs
+++ src/fileinfo.rs
@@ -25,6 +25,15 @@ impl Fileinfo {
     pub fn start_analysis(&self, args: &AnalysisArguments) -> Result<Analysis> {
         let mut conn = self.conn_factory.new_connection();
         let url = format!("{}/fileinfo/analyses", conn.api_url());
+        let api_args = self.create_api_args(args)?;
+        let response = conn.send_post_request(&url, &api_args)
+            .chain_err(|| "failed to start an analysis")?;
+        let content = response.body_as_json()?;
+        let id = content["id"].as_str().unwrap();
+        Ok(Analysis::new(id, conn))
+    }
+
+    fn create_api_args(&self, args: &AnalysisArguments) -> Result<APIArguments> {
         let mut api_args = APIArguments::new();
         api_args.add_opt_string_arg("output_format", args.output_format());
         api_args.add_opt_bool_arg("verbose", args.verbose());
@@ -36,10 +45,6 @@ impl Fileinfo {
                 bail!("no input file");
             },
         }
-        let response = conn.send_post_request(&url, &api_args)
-            .chain_err(|| "failed to start an analysis")?;
-        let content = response.body_as_json()?;
-        let id = content["id"].as_str().unwrap();
-        Ok(Analysis::new(id, conn))
+        Ok(api_args)
     }
 }
